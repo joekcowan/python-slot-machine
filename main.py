967dffee5a53b2a7d4b14485a86e804a93ebd351
joekcowan <joekcowan@gmail.com>
2025-02-05 18:15:19 -0500

    update

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
 
+ROWS = 3
+COLS = 3
+
+symbolCount = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
 # function for getting slot deposit
 def deposit():
     while True:
